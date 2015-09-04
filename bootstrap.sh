10a4cb108135bc26a082cc77d57b420c9bcf42c2
graeme.sutherland <graeme.sutherland@pure360.com>
2015-09-04 17:22:16 +0100

    better bootstrapper

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-git pull
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-}
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
-unset doIt
-source ~/.bash_profile
+#
+# Start from repo base
+# Give destination path as arg
+# Note this will delete symlinks and move existing files out of the way.
 
+# Where to place moved old files
+OUT_OF_THE_WAY=./olddotfiles
+
+# Where to link files from
+SOURCE_PATH=`pwd`
+
+# Files to link
+FILES=".vimrc
+.vim
+.gvimrc
+.tmux.conf"
+
+cd $1 || exit 1
+
+mkdir -p "$OUT_OF_THE_WAY"
+
+##
+## Move given filename out of the way, and symbolic link
+## in new files.
+moveAndLink() {
+  [ -h $1 ] && rm $1
+  [ -a $1 ] && mv $1 $OUT_OF_THE_WAY/$1
+  ln -s $SOURCE_PATH/$1 $1
+}
+
+##
+## Loop
+##
+
+for f in $FILES
+do
+  moveAndLink "$f"
+done
